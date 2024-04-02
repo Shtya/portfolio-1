55e41744fbc3b3a102e8ce987c7fba169ba0ef77
Shtya <115083423+Shtya@users.noreply.github.com>
2024-04-02 06:11:57 +0200

    1

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,9 +1,8 @@
 'use client'
 import React from 'react'
-import wave from '@/assets/wave.svg'
-// import cv from '@/assets/cv.pdf'
 import Image from 'next/image'
 import Img from '@/assets/me.jpg'
+
 
 const About = () => {
   return (
@@ -15,7 +14,7 @@ const About = () => {
           <p data-aos='fade-left' >I like building Web Applications that leverage my skills in web development. Check out some of my work in the Projects section. I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.  I'm a Freelance Fullstack Developer with over 3 years of experience. </p>
           <div className="groub-btn">
             <a href='#contact'  data-aos='fade-left' className="btn"> Hire me </a>
-            <a href="" target='_blank' download  data-aos='fade-left' className="btn"> download CV </a>
+            <a href="cv.pdf" target='_blank' download  data-aos='fade-left' className="btn"> download CV </a>
           </div>
         </div>
       </div>
